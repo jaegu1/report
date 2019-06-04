36aadb61da46841e75978feb37db9acaa3299e9c
jaegu1 <50862534+jaegu1@users.noreply.github.com>
2019-06-04 23:16:19 +0900

    Add files via upload

---
+++ 개인/opengl/opengl.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <GL/glut.h>
+
+
+void display() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0f, .0f, .0f);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(-0.5f, 0.5f);
+	glEnd();
+	glFinish();
+
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutCreateWindow("OpenGL");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
+
